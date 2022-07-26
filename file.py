4fc8a66ca83847d04a2316ed5ee39309c7c5d888
dapk <danushreepremkumar@gmail.com>
2022-07-26 12:28:04 +0200

    JIR-5 #commment The file is midified from my branch-5 #progress

--- file.py
+++ file.py
@@ -4,3 +4,6 @@ print("This is JIGIT")
 print("The 1st edit in my JIGIT branch-4")!!!
 print("edit for no reason") 
 print("20-07-2022")!! "" @@
+
+Working in JIR-5 branch
+print("the branched re working in order")
