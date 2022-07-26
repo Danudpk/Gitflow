d546ce9d5098628469c5a4af4ae0c61a5ae68b8e
dapk <danushreepremkumar@gmail.com>
2022-07-26 11:56:49 +0200

    JIR-4 #time 3h 30m 20s Time logged

--- file.py
+++ file.py
@@ -2,4 +2,5 @@ print("this is my file")
 print("Hi ALL")
 print("This is JIGIT")
 print("The 1st edit in my JIGIT branch-4")!!!
-print("edit for no reason")
+print("edit for no reason") 
+print("20-07-2022")
